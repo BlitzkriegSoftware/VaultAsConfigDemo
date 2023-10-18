111e1deb7b921d158929c7c6b9962e67f4463728
Stuart.Williams <spookdejur@hotmail.com>
2023-10-17 20:15:25 -0700

    working w. hashicorpupdate

--- scripts/start_docker_vault.ps1
+++ scripts/start_docker_vault.ps1
@@ -10,7 +10,11 @@ $port=8200
 
 docker stop ${imagename} 2>&1 | out-null
 docker rm ${imagename} 2>&1 | out-null
-docker pull vault
+docker pull hashicorp/vault
 
 Write-Output "Vault UI: $($Env:VAULT_ADDR)/ui use token of $($Env:VAULT_TOKEN) to login"
-docker run -d --cap-add=IPC_LOCK -p "${port}:${port}" -e "VAULT_DEV_ROOT_TOKEN_ID=$($Env:VAULT_TOKEN)" --name=${imagename} vault
+docker run -d `
+	--cap-add=IPC_LOCK `
+	-p "${port}:${port}" `
+	-e "VAULT_DEV_ROOT_TOKEN_ID=$($Env:VAULT_TOKEN)" `
+	--name=${imagename} hashicorp/vault
